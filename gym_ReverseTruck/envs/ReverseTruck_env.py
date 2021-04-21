7eed3ceec0ac6fcce46159e27c2d1dc9b15262f3
aharrisonau <48994034+aharrisonau@users.noreply.github.com>
2021-04-22 00:34:12 +0800

    Update ReverseTruck_env.py

--- gym_ReverseTruck/envs/ReverseTruck_env.py
+++ gym_ReverseTruck/envs/ReverseTruck_env.py
@@ -76,7 +76,7 @@ class ReverseTruckEnv(gym.Env):
     self.state = [pivX,pivY,pmAng,trlAng]
 
     if abs(pivX - 0) <= 0.5 and \
-              abs(pivY - TruckDefinition[2]) <= 0.5 and \
+              abs(pivY - self.TruckDefinition[2]) <= 0.5 and \
               abs(trlAng) <= 0.174 : # trailer within 10deg of straight
       reward = 1.0
     else:
